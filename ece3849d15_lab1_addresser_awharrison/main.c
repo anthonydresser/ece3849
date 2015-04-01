5a5e5aba1c55d5a3b4c51a03b953e23acebd0efb
Anthony Dresser <smiley.yellow@gmail.com>
2015-04-01 16:58:01 -0400

    Anthony & Abby: Lab 1

--- ece3849d15_lab1_addresser_awharrison/main.c
+++ ece3849d15_lab1_addresser_awharrison/main.c
@@ -21,7 +21,10 @@
 // #define BUTTON_CLOCK 200 // button scanning interrupt rate in Hz
 #define ADC_BUFFER_SIZE 2048 // must be a power of 2
 #define ADC_BUFFER_WRAP(i) ((i) & (ADC_BUFFER_SIZE - 1)) // index wrapping macro
-#define SCREEN_SIZE	128
+#define	ADC_BITS	10
+#define PIXELS_PER_DIV	50
+#define	VIN_RANGE	6
+#define ADC_OFFSET	512
 
 // Global Variables
 unsigned long g_ulSystemClock; // system clock frequency in Hz
@@ -29,19 +32,30 @@ volatile int g_iADCBufferIndex = ADC_BUFFER_SIZE - 1; // latest sample index
 volatile unsigned short g_pusADCBuffer[ADC_BUFFER_SIZE]; // circular buffer
 volatile unsigned long g_ulADCErrors = 0; // number of missed ADC deadlines
 
+#define FIFO_SIZE 10		// FIFO capacity is 1 item fewer
+typedef char DataType;		// FIFO data type
+volatile DataType fifo[FIFO_SIZE];	// FIFO storage array
+volatile int fifo_head = 0;	// index of the first item in the FIFO
+volatile int fifo_tail = 0;	// index one step past the last item
+
 // Function Prototypes
 void TimerISR(void);
 void ADC_ISR(void);
+int fifo_put(DataType data);
+int fifo_get(DataType *data);
 
 int main(void) {
 
 	// Local Variables
 	int trigger_val = 512; // 10-bit ADC
+	float fVoltsPerDiv = .8;
 	int temp_index;
-//	unsigned short temp_buffer[ADC_BUFFER_SIZE];
-	int p_buffer[SCREEN_SIZE]; // number of pixles that can be on the screen at one time
+	int y;
+	int prevy;
+	float fScale;
+	//	unsigned short temp_buffer[ADC_BUFFER_SIZE];
+	int p_buffer[FRAME_SIZE_X]; // number of pixles that can be on the screen at one time
 	int i, j;
-	char pcStr[200]; // string buffer
 	// initialize the clock generator
 	if (REVISION_IS_A2)
 		SysCtlLDOSet(SYSCTL_LDO_2_75V);
@@ -60,6 +74,33 @@ int main(void) {
 	// initializes SSI/SPI interface to OLED to 3.5MHz clock
 
 	// initialization code
+	// initialize a general purpose timer for periodic interrupts
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0); // enables TIMER0
+	TimerDisable(TIMER0_BASE, TIMER_BOTH);
+	// TIMER0_BASE is the base address of the timer module
+	// TIMER_BOTH disables TIMER_A and TIMER_B
+	TimerConfigure(TIMER0_BASE, TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PERIODIC);
+	// configures timer
+	// TIMER0_BASE is the base address of the timer module
+	// TIMER_CFG_SPLIT_PAIR -> two half-width timers, must configure timers separately
+	// TIMER_CFG_A_PERIODIC -> sets TIMER_A to a half-width timer
+
+	// prescaler for a 16-bit timer
+	ulPrescaler = (g_ulSystemClock / BUTTON_CLOCK - 1) >> 16;
+	// system frequency div by 199 right shifted 16 bits
+
+	// 16-bit divider (timer load value)
+	ulDivider = g_ulSystemClock / (BUTTON_CLOCK * (ulPrescaler + 1)) - 1;
+	TimerLoadSet(TIMER0_BASE, TIMER_A, ulDivider);
+	// if the timer is running, then the ulDivider is immediately loaded into the timer
+	TimerPrescaleSet(TIMER0_BASE, TIMER_A, ulPrescaler);
+	// ulPrescaler must be between 0 and 255 for 16/32-bit timers
+	TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+	// enables interrupt when TIMER A times out aka every 10ms
+	TimerEnable(TIMER0_BASE, TIMER_A);
+
+	IntPrioritySet(INT_TIMER0A, 32); // 0 = highest priority, 32 = next lower
+	IntEnable(INT_TIMER0A);
 
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0); // enable the ADC
 	SysCtlADCSpeedSet(SYSCTL_ADCSPEED_500KSPS); // specify 500ksps
@@ -101,19 +142,30 @@ int main(void) {
 			GPIO_PIN_TYPE_STD_WPU);
 
 	while (true) {
+		DataType c;
+		if(fifo_get(&c)){
+			switch(c){
+			case 'U':
+			}
+		}
 		FillFrame(0);
 		// half screen width behind most recent sample is sample 1984
 		// 1/2 ADC units is 512
 		temp_index = g_iADCBufferIndex;
-		for(i = ADC_BUFFER_WRAP(temp_index - 64); i != (temp_index - 1024 - 64); i--) {
+		for(i = ADC_BUFFER_WRAP(temp_index - FRAME_SIZE_X/2); i != (temp_index - 1024 - FRAME_SIZE_X/2); i--) {
 			if((g_pusADCBuffer[ADC_BUFFER_WRAP(i)] < trigger_val) && (g_pusADCBuffer[ADC_BUFFER_WRAP(i + 1)] > trigger_val)) {
-				for(j = 0; j < SCREEN_SIZE; j++) {
+				for(j = 0; j < FRAME_SIZE_X; j++) {
 					p_buffer[j] = g_pusADCBuffer[ADC_BUFFER_WRAP(i - 63  + j)];
 				}
 			}
 		}
-		for(i = 0; i < SCREEN_SIZE; i++) {
-			DrawPoint(j, p_buffer[i]/96, 15);
+		fScale = (VIN_RANGE * PIXELS_PER_DIV)/((1 << ADC_BITS) * fVoltsPerDiv);
+		prevy = FRAME_SIZE_Y/2 - (int)round((p_buffer[0] - ADC_OFFSET) * fScale);
+		for(i = 1; i < FRAME_SIZE_X; i++) {
+			y = FRAME_SIZE_Y/2 - (int)round((p_buffer[i] - ADC_OFFSET) * fScale);
+			//			DrawPoint(i, y, 15);
+			DrawLine(i-1, prevy, i, y, 15);
+			prevy = y;
 		}
 		RIT128x96x4ImageDraw(g_pucFrame, 0, 0, FRAME_SIZE_X, FRAME_SIZE_Y);
 	}
@@ -133,4 +185,63 @@ void ADC_ISR(void) {
 	g_iADCBufferIndex = buffer_index;
 }
 
+// put data into the FIFO, skip if full
+// returns 1 on success, 0 if FIFO was full
+int fifo_put(DataType data)
+{
+	int new_tail = fifo_tail + 1;
+	if (new_tail >= FIFO_SIZE) new_tail = 0; // wrap around
+	if (fifo_head != new_tail) {	// if the FIFO is not full
+		fifo[fifo_tail] = data;		// store data into the FIFO
+		fifo_tail = new_tail;		// advance FIFO tail index
+		return 1;					// success
+	}
+	return 0;	// full
+}
 
+// get data from the FIFO
+// returns 1 on success, 0 if FIFO was empty
+int fifo_get(DataType *data)
+{
+	if (fifo_head != fifo_tail) {	// if the FIFO is not empty
+		*data = fifo[fifo_head];	// read data from the FIFO
+		if(fifo_head +1 >= FIFO_SIZE)
+			fifo_head = 0;
+		else
+			fifo_head++;
+		return 1;					// success
+	}
+	return 0;	// empty
+}
+
+void TimerISR(void) {
+	unsigned long presses = g_ulButtons;
+	TIMER0_ICR_R = TIMER_ICR_TATOCINT; // clear interrupt flag
+	ButtonDebounce(
+			//up button
+			(~GPIO_PORTE_DATA_R & GPIO_PIN_0) +
+			//left button
+			(~GPIO_PORTE_DATA_R & GPIO_PIN_2) +
+			//right button
+			(~GPIO_PORTE_DATA_R & GPIO_PIN_3) +
+			//down button
+			(~GPIO_PORTE_DATA_R & GPIO_PIN_1) +
+			//select button
+			(~GPIO_PORTF_DATA_R & GPIO_PIN_1) << 3);
+	presses = ~presses & g_ulButtons; // button press detector
+	if (presses & 1) { // up button pressed
+		fifo_put('U');
+	}
+	if (presses & 2) { // down button
+		fifo_put('D');
+	}
+	if (presses & 4) { // left button
+		fifo_put('L');
+	}
+	if (presses & 8) { // right button
+		fifo_put('R');
+	}
+	if (presses & 16) { //select button
+		fifo_put('S');
+	}
+}
