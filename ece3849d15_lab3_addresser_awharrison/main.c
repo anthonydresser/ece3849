4c74ce90db95557e75807ed6f39edf7349d7ea2d
Abby <valkyrie0528@gmail.com>
2015-04-29 19:11:26 -0400

    more lab 3

--- ece3849d15_lab3_addresser_awharrison/main.c
+++ ece3849d15_lab3_addresser_awharrison/main.c
@@ -26,6 +26,7 @@
  *  ======== main ========
  */
 Void main() {
+	IntMasterDisable();
 
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_COMP0);
 	ComparatorConfigure(COMP_BASE, 0, COMP_TRIG_NONE|COMP_INT_HIGH|COMP_ASRCP_REF);
@@ -50,8 +51,9 @@ Void main() {
 	TimerIntEnable(TIMER0_BASE, TIMER_CAPA_EVENT);
 	TimerEnable(TIMER0_BASE, TIMER_A);
     
-    IntPrioitySet(INT_TIMER0A, 0);
     IntEnable(INT_TIMER0A);
+
+    IntMasterEnable();
 
 	BIOS_start();     /* enable interrupts and start SYS/BIOS */
 }
